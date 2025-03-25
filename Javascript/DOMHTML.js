58cbf190b33f76da54078384068feed8f517c3e0
Pratik <pratikathare6@gmail.com>
2025-03-25 19:51:30 +0530

    DOM

--- Javascript/DOMHTML.js
+++ Javascript/DOMHTML.js
@@ -5,3 +5,22 @@ document.querySelectorAll('#fpara');
 
 
 let elem = document.querySelector('#fpara');
+
+let fheading = document.createElement('h1');
+
+fheading.textContent='hello ji';
+
+// let bodyTag  = document.querySelector('body');
+
+// bodyTag.appendChild(fheading);
+
+// let mydiv = document.querySelector('#mydiv');
+// let newelement = document.createElement('span');
+// newelement.textContent='my name is john'
+// mydiv.insertAdjacentHTML('beforebegin', newelement);
+// mydiv.insertAdjacentele('beforebegin', newelement);
+// mydiv.insertAdjacentElement('beforebegin', newelement);
+// let bodyTag  = document.querySelector('body');
+// bodyTag.appendChild(fheading);
+
+
