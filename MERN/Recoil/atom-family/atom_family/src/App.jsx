a17c57135350a3ad682ceb3b9b320f1140fcbb79
Pratik <pratikathare6@gmail.com>
2025-09-13 19:42:10 +0530

    atom family

---
+++ MERN/Recoil/atom-family/atom_family/src/App.jsx
@@ -0,0 +1,29 @@
+ 
+import './App.css'
+import { RecoilRoot,useRecoilValue } from 'recoil'
+import { todosAtomFamily  } from './atom'
+function App() { 
+  return (
+    <RecoilRoot>
+      <Todo id={1} />
+      <Todo id={2} />
+
+    </RecoilRoot>
+  )
+}
+
+function Todo({id}){
+
+    const currenttodo = useRecoilValue(todosAtomFamily(id))
+
+      return(
+        <div>
+        {currenttodo.title }
+        {currenttodo.description}
+
+        </div>
+     
+      )
+}
+
+export default App
