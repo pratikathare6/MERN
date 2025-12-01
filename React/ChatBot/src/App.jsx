4d9385cc487fb34846dac392f97c1b83c5c1002f
Pratik Athare <pratik@Pratiks-MacBook-Air.local>
2025-12-01 12:43:55 +0530

    Chatbot

--- React/ChatBot/src/App.jsx
+++ React/ChatBot/src/App.jsx
@@ -1,18 +1,39 @@
+import { ChatInput, ChatMessage } from "./chatbot";
 
-import {ChatInput,ChatMessage} from './chatbot'
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
+  const chatmessages = [
+    {
+      message: "Hello Chatbot",
+      sender: "User",
+    },
+     {
+      message: "Hey",
+      sender: "robot",
+    }
+  ];
+
+      chatmessages.map((ChatMessage)=>{
+
+          return(
+
+            <ChatMessage
+
+             message={ChatMessage.message}
+             sender={ChatMessage.sender}
+
+            />
+          )
+      })
 
   return (
     <>
-      <ChatInput/>
-      <ChatMessage/>
-     
+      <ChatInput />
+      <ChatMessage message="Hello ChatBot" sender="user" />
+      <ChatMessage message="hey" sender="robot" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- React/ChatBot/src/chatbot.jsx
+++ React/ChatBot/src/chatbot.jsx
@@ -1,32 +1,49 @@
-
-export function ChatInput(){
-
-        return(
-
-            <div>
-
-                <input placeholder="Send a message to ChatBot"/>
-                <button>Send</button>
-            </div>
+export function ChatInput() {
+  return (
+    <div>
+      <input placeholder="Send a message to ChatBot"
+             size="30"
+       />
 
 
-        )
-
-
-
+      <button>Send</button>
+    </div>
+  );
 }
 
-export function ChatMessage(){
+export function ChatMessage({ message, sender }) {
+  //const message = props.message;
+  //const sender = props.sender;
 
-    return(
-        <div>
+  //const {message,sender} = props; //destructuring
 
-             Hello Chatbot
-             {/* <img src="https://img.icons8.com/?size=100&id=TUk7vxvtu6hX&format=png&color=000000" alt="img" /> */}
-             <img src="https://img.icons8.com/?size=100&id=xXjlE05o3dcg&format=png&color=000000" alt="" />
+  /*if (sender === "robot") {
+    return (
+      <div>
+        
+        <img
+          src="https://img.icons8.com/?size=100&id=TUk7vxvtu6hX&format=png&color=000000"
+          alt=""
+        />
+        {message}
+      </div>
+    );
+  }*/
 
+  return (
+    <div>
+      {sender === "robot" && (
+        <img
+          src="https://img.icons8.com/?size=100&id=TUk7vxvtu6hX&format=png&color=000000"
+          alt=""
+        />
+      )}
 
-        </div>
-    )
+      {message}
+      { sender ==="user" && <img
+        src="https://img.icons8.com/?size=100&id=xXjlE05o3dcg&format=png&color=000000"
+        alt=""
+      />}
+    </div>
+  );
 }
- 
