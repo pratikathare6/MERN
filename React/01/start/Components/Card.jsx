75eb0f9b1e75854e589e683683386614c0d90ca7
Pratik Athare <pratik@Pratiks-MacBook-Air.local>
2026-01-14 15:48:08 +0530

    Starting react from basics

---
+++ React/01/start/Components/Card.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export const Card = (props) => {
+  return (
+    <div className='card'>
+       
+       
+        <div className='topbar'>
+          <img src={props.link} alt="imgigmg" />
+          <button><span class="IconContainer">
+          <svg viewBox="0 0 384 512" height="0.9em" class="icon">
+      <path
+        d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"
+      ></path>
+      </svg>
+      </span>
+      <p class="text">Save</p></button>
+
+
+        </div>
+
+        <div className='titles'>
+        <div className='title'>{props.title}</div>
+        <div className='title2'>{props.title2}</div>
+        </div>
+
+        <div className='subtitle'>{props.subtitle}</div>
+        <div className='buttons'>
+          <button>Contract</button>
+          <button>Remote</button>
+        </div>
+
+        <div className='line'></div>
+        <div className='bottombar'>
+          <div className='bottombarleft'>
+          <div className='rate'>{props.rate}</div>
+          <div className='location'>{props.location}</div>
+          </div>
+          <button>Apply Now</button>
+        </div>
+
+
+
+    </div>
+  )
+}
